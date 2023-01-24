534d1157e05f2be89722f1de4c698d5da79c3cf1
Andjela Prosenica <andjelaprosenica@gmail.com>
2023-01-24 11:47:44 +0100

    Inicijalizovani kontroleri sa metodama

---
+++ app/Http/Controllers/ArchitectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Architect;
+use Illuminate\Http\Request;
+
+class ArchitectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Architect  $architect
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Architect $architect)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Architect  $architect
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Architect $architect)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Architect  $architect
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Architect $architect)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Architect  $architect
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Architect $architect)
+    {
+        //
+    }
+}
